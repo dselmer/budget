af951242a9d3a788501aed288e43c985b04c6500
dselmer <dselmer@icstars.org>
2023-05-06 19:36:07 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Header, Segment, Statistic,Grid } from "semantic-ui-react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Header as="h1">Budget</Header>
+        <Statistic size="small">
+          <Statistic.Label>your balance:</Statistic.Label>
+          <Statistic.Value>2,550.53</Statistic.Value>
+        </Statistic>
+        <Segment textAlign="center">
+          <Grid columns={2} divided>
+            <Grid.Row>
+              <Grid.Column>
+                <Statistic size="tiny" color='green'>
+                  <Statistic.Label style={{textAlign:'left'}}>
+                    Incoming:
+                  </Statistic.Label>
+                  <Statistic.Value>1,045.50</Statistic.Value>
+                </Statistic>
+              </Grid.Column>
+              <Grid.Column>
+              <Statistic size="tiny" color='red'>
+                  <Statistic.Label style={{textAlign:'left'}}>
+                    Expenses:
+                  </Statistic.Label>
+                  <Statistic.Value>625.50</Statistic.Value>
+                </Statistic>
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Segment>
+      </Container>
     </div>
   );
 }
